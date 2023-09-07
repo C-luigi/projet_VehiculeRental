8c4cfccb1ada2bbdc37e54ef815d12f5c4ddcecc
Roche <michel.roche@le-campus-numerique.fr>
2023-09-07 16:07:41 +0200

    gestion modèle

--- src/main/java/com/vehiculerental/locationsDeVehicules/model/Car.java
+++ src/main/java/com/vehiculerental/locationsDeVehicules/model/Car.java
@@ -1,4 +1,32 @@
 package com.vehiculerental.locationsDeVehicules.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+@Entity
+@Table(name="Car")
 public class Car {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int idCar;
+    @Column(name="registration")
+    private String registration;
+    @Column(name="brand")
+    private String brand;
+    @Column(name="model")
+    private String model;
+    @Column(name="color")
+    private String color;
+    @Column(name="price_reservation")
+    private int price_reservation;
+    @Column(name="price_wear")
+    private int price_wear;
+    @Column(name="fiscal_horse")
+    private String fiscal_horse;
+    @Column(name="disponibility")
+    private String  disponibility;
+
 }
--- src/main/java/com/vehiculerental/locationsDeVehicules/model/Customer.java
+++ src/main/java/com/vehiculerental/locationsDeVehicules/model/Customer.java
@@ -1,4 +1,23 @@
 package com.vehiculerental.locationsDeVehicules.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.xml.crypto.Data;
+
 public class Customer {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int idCustomer;
+    @Column(name="name")
+    private String name;
+    @Column(name="first_name\t")
+    private String first_name	;
+    @Column(name="birth_date")
+    private Data birth_date;
+    @Column(name="permit")
+    private int permit;
+
 }

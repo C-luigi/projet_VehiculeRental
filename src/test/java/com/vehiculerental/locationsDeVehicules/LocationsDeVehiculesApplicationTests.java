249f5882fdc10d97728a203e7b9566aec3be6af6
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-09-07 11:39:26 +0200

    first commit

---
+++ src/test/java/com/vehiculerental/locationsDeVehicules/LocationsDeVehiculesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehiculerental.locationsDeVehicules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationsDeVehiculesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
